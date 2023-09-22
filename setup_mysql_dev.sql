65069c9b2b4c73218ee5dc79b0ddc8af2623fe95
kry01 <brahimsecond@gmail.com>
2023-09-22 11:08:00 -0700

    fixed

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,7 +1,13 @@
 -- Create dev database and user
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
 -- Give Permissions to User
+
 GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+
 GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
